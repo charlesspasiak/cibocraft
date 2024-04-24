746d73dd7be0f0a7c95f47af8de77d067283ef21
charlespasiak <charlesspasiak@gmail.com>
2024-04-24 14:30:21 +0700

    installing bundle analyzer

--- webpack.common.js
+++ webpack.common.js
@@ -4,6 +4,7 @@ const path = require('path');
 const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 const ImageminWebpackPlugin = require('imagemin-webpack-plugin').default;
 const ImageminMozjpeg = require('imagemin-mozjpeg');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
   entry: {
@@ -73,5 +74,6 @@ module.exports = {
         }),
       ],
     }),
+    new BundleAnalyzerPlugin(),
   ],
 };
